f804f7c7450133384a5625390dc035df1b02cbc2
Ander <anderson.blandon@hotmail.com>
2025-03-19 10:04:11 -0500

    Primer commit: subiendo código de numpy

---
+++ Main.py
@@ -0,0 +1,19 @@
+import numpy as np  
+
+
+array = np.array([10, 20, 30, 40, 50])  
+
+
+print("Array de NumPy:", array)
+
+sum_array = np.sum(array) 
+mean_array = np.mean(array) 
+std_array = np.std(array) 
+
+Resultado1 = f"El resultado del array es: {sum_array}"
+Resultado2 = f"La media del array es: {mean_array}"
+Resultado3 = f"La media del array es: {std_array}"
+
+print(Resultado1)
+print(Resultado2)
+print(Resultado3)
